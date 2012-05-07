91d7931012203c492fc15ccf8e6462bd9eb10106
Wellington Ferreira <wellington.sferreira@hotmail.com>
2012-05-07 00:56:12 -0300

    Inclusao da pasta bin

---
+++ bin/scrollable-elements.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+function scrollBotoesLaterais(bar,elementoDelimitador){
+	
+	   var limiteSuperior = elementoDelimitador.position().top;
+	   var limiteInferior = elementoDelimitador.position().top + elementoDelimitador.height() - bar.height();
+	   
+     $(window).scroll(function (event) {
+    	 
+   	   var limiteSuperior = elementoDelimitador.position().top;
+   	   var limiteInferior = elementoDelimitador.position().top + elementoDelimitador.height() - bar.height();
+   	   var y = $(this).scrollTop()+$(this).height();
+           
+         if($(this).scrollTop()> limiteSuperior && $(this).scrollTop() < limiteInferior ){
+       	   bar.css('position','fixed');
+       	   bar.css('top','0');
+         }else if($(this).scrollTop()> limiteSuperior){
+       	   bar.css('position','relative');
+       	   bar.css('top',parseInt(elementoDelimitador.height())-parseInt(bar.height()));
+         }else{
+        	 bar.css('position','relative');
+         }
+     });
+}
